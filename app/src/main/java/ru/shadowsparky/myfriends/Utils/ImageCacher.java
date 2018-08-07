b45e803a1dba22f33a0b8965731f522af5fd44c7
Eugene <samsonov_eugene@mail.ru>
2018-08-07 23:26:45 +0300

    Исправление ошибок, повышение производительности

--- app/src/main/java/ru/shadowsparky/myfriends/Utils/ImageCacher.java
+++ app/src/main/java/ru/shadowsparky/myfriends/Utils/ImageCacher.java
@@ -9,6 +9,7 @@ import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -16,7 +17,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class ImageCacher {
-    private final String path = Environment.getExternalStorageDirectory() + "/test/";
+    private final String path = Environment.getExternalStorageDirectory().toString() + File.separator + "MyFriendsCache" + File.separator;
     private static ImageCacher instance;
 
     public static ImageCacher getInstance() {
@@ -25,52 +26,46 @@ public class ImageCacher {
         }
         return instance;
     }
-    // fixme
+
     public Boolean saveImage(String imageName, Bitmap image) {
-        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
-        File folder = new File(extStorageDirectory, "MyFriendsCache");
-        if (!folder.exists())
-            folder.mkdir();
-    // fixme
-        File file = new File(folder.getAbsolutePath(), imageName);
-        try {
-            file.getParentFile().mkdirs();
-            if (!file.createNewFile()) {
-                Log.println(Log.DEBUG, "MAIN_TAG", "file don't created " + file.getAbsolutePath());
-            } else {
-                Log.println(Log.DEBUG, "MAIN_TAG", "FILE CREATED " + file.getAbsolutePath());
+        if (createCacheFolder()) {
+            File file = new File(path, imageName);
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.PNG, 0, bytes);
+            try {
+                writeToFile(file, bytes);
+                return true;
+            } catch (IOException e) {
+                Log.println(Log.DEBUG, "MAIN_TAG", "Exception on image save: " + e.toString());
             }
-        } catch (IOException e) {
-            Log.println(Log.DEBUG, "MAIN_TAG", "ERROR: " + e.toString());
         }
-        // fixme
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-//        file = new File(Environment.getExternalStorageDirectory() + File.separator + imageName);
-        try {
-            //fixme fixme fixme fixme fixme fixme
-            FileOutputStream fo = new FileOutputStream(file);
-            fo.write(bytes.toByteArray());
-            fo.flush();
-            fo.close();
-            Log.println(Log.DEBUG, "MAIN_TAG", "Image saved: " + file.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.println(Log.DEBUG, "MAIN_TAG", e.toString());
-        }
-//            FileOutputStream out = new FileOutputStream(file);
-//            image.compress(Bitmap.CompressFormat.PNG, 100, out);
-//            out.write();
-//            out.flush();
-//            out.close();
-//        return true;
-        // fixme
         return false;
     }
 
+    public Boolean createCacheFolder() {
+        File folder = new File(path, "MyFriendsCache");
+        if (!folder.exists()) {
+            if (!folder.mkdir()) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public void writeToFile(File file, ByteArrayOutputStream bytes) throws IOException {
+        FileOutputStream fo = new FileOutputStream(file);
+        fo.write(bytes.toByteArray());
+        fo.flush();
+        fo.close();
+        Log.println(Log.DEBUG, "MAIN_TAG", "Image saved: " + file.getAbsolutePath());
+
+    }
+
     // fixme
+    // на самом деле тут все шикарно.
     public String getImageName(String fullname) {
-        String test []= fullname.split("/");
         String result = fullname.split("/")[3];
         if (result.equals("images")) {
             result = fullname.split("/")[4];
