001ab45b1db8d1024b32850d788d3174694301e0
Eugene <samsonov_eugene@mail.ru>
2018-08-13 18:41:19 +0300

    Попытка тестирования

--- app/src/test/java/ru/shadowsparky/myfriends/Utils/ImageCacherTest.java
+++ app/src/test/java/ru/shadowsparky/myfriends/Utils/ImageCacherTest.java
@@ -1,57 +1,54 @@
 package ru.shadowsparky.myfriends.Utils;
-import android.graphics.Bitmap;
 
-import com.vk.sdk.api.model.VKApiPhoto;
 
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.assertNotEquals;
 
+// FIXME Зависимости андроида не позволяют тестировать этот класс.
 public class ImageCacherTest {
-    VKApiPhoto photoWithHD = new VKApiPhoto();
-    VKApiPhoto photoWithoutHD = new VKApiPhoto();
-    ImageCacher cacher = new ImageCacher();
-    Bitmap picture;
-    @Before
-    public void setUp() throws Exception {
-        photoWithHD.photo_1280 = "https://sun1-17.userapi.com/c635106/v635106426/88ec/jWEzaw-d8Xs.jpg";
-        photoWithoutHD.photo_1280 = "";
-        photoWithoutHD.photo_604 = "https://sun1-17.userapi.com/c635106/v635106426/88ec/jWEzaw-d8Xs.jpg";
-    }
-
-    @Test
-    public void hdPhotoChecker() {
-        String result = cacher.hdPhotoChecker(photoWithHD);
-        assertNotEquals(result, "");
-        result = cacher.hdPhotoChecker(photoWithoutHD);
-        assertNotEquals(result, "");
-    }
-
-    @Test
-    public void getImage() {
-        picture = cacher.getImage("5ND-FPyQe_U.jpg");
-    }
-
-    @Test
-    public void saveImage() {
-        //;
-    }
-
-    @Test
-    public void createCacheFolder() {
-    }
-
-    @Test
-    public void writeToFile() {
-    }
-
-    @Test
-    public void getParsedName() {
-    }
-
-    @Test
-    public void checkFileExists() {
-    }
-
-
+//    VKApiPhoto photoWithHD = new VKApiPhoto();
+//    VKApiPhoto photoWithoutHD = new VKApiPhoto();
+//    ImageCacher cacher = new ImageCacher();
+//    Bitmap picture;
+//    @Before
+//    public void setUp() throws Exception {
+//        photoWithHD.photo_1280 = "https://sun1-17.userapi.com/c635106/v635106426/88ec/jWEzaw-d8Xs.jpg";
+//        photoWithoutHD.photo_1280 = "";
+//        photoWithoutHD.photo_604 = "https://sun1-17.userapi.com/c635106/v635106426/88ec/jWEzaw-d8Xs.jpg";
+//    }
+//
+//    @Test
+//    public void hdPhotoChecker() {
+//        String result = cacher.hdPhotoChecker(photoWithHD);
+//        assertNotEquals(result, "");
+//        result = cacher.hdPhotoChecker(photoWithoutHD);
+//        assertNotEquals(result, "");
+//    }
+//
+//    @Test
+//    public void getImage() {
+//        cacher.getImage("test.png");
+//
+//    }
+//
+//    @Test
+//    public void saveImage() {
+//        ;
+//    }
+//
+//    @Test
+//    public void createCacheFolder() {
+//    }
+//
+//    @Test
+//    public void writeToFile() {
+//    }
+//
+//    @Test
+//    public void getParsedName() {
+//    }
+//
+//    @Test
+//    public void checkFileExists() {
+//    }
+//
+//
 }
---
+++ app/src/test/java/ru/shadowsparky/myfriends/Utils/ImageDownloaderTest.java
@@ -0,0 +1,28 @@
+package ru.shadowsparky.myfriends.Utils;
+
+import android.media.Image;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+// FIXME Стандартными средствами asynctask не тестируется.
+public class ImageDownloaderTest {
+//    ICallbacks.IDownloadImage callback;
+//    @Before
+//    public void setUp() throws Exception {
+//        callback = ((image, url) -> {
+//            System.out.println("Image downloaded: " + url);
+//        });
+//    }
+//
+//    @Test
+//    public void executeDownload() throws ExecutionException, InterruptedException {
+//        ImageDownloader downloader = mock(ImageDownloader.class);
+//        downloader.execute("https://pp.userapi.com/c845221/v845221684/b568f/GCrWm1TEfBc.jpg");
+//    }
+}
